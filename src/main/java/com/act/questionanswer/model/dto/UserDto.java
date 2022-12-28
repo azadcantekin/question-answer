8f5ec95dfafd7ebc20b8e81045303f05b2615915
Azad Can Tekin <tekin.act@gmail.com>
2022-12-28 13:11:19 +0300

    Getting started .

---
+++ src/main/java/com/act/questionanswer/model/dto/UserDto.java
@@ -0,0 +1,25 @@
+package com.act.questionanswer.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String gender;
+    private String role;
+    private List<QuestionDto> questionDtoList;
+    private List<AnswerDto> answerDtoList;
+
+
+}
